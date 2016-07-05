77a99a5fe424064ec8f5a9da6af1d446473d2873
Yuwei <keyfoxth-github@outlook.com>
2016-07-05 10:45:54 +0800

    Update

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -6,8 +6,10 @@ import './style'
 export default () => (
   <header>
     <h1 className="name">Keyfoxth</h1>
-    <Link to="/" className="me">
-      <img src="https://avatars0.githubusercontent.com/u/10647132" />
-    </Link>
+    <section className="me">
+      <Link to="/">
+        <img src="https://avatars0.githubusercontent.com/u/10647132" />
+      </Link>
+    </section>
   </header>
 )
